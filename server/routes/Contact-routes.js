31fb30c15b4917febb8b7c84fd4110f0c4ffe422
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2024-07-07 19:17:34 +0530

    Delete server directory

--- server/routes/Contact-routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const ContactForm = require("../controllers/Contact-controller")
-const router = express.Router()
-
-router.route("/contact").post(ContactForm);
-module.exports=router;
--- server/routes/service-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const services = require("../controllers/service-controller.js");
-const router = express.Router();
-
-router.route("/service").get(services);
-
-module.exports = router;
